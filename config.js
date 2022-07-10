114acc7528267fbfa2cc02cb74c2587e0a9b7337
cev862 <108903931+cev862@users.noreply.github.com>
2022-07-10 10:04:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbzgVsgg7WvleOBydGAiX1vSnjp9RDojJqTH_FVPDBTMMdUzvZrZUmMJ_lTuTuhIHEBg/exec";
